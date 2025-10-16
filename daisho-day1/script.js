861bb5cc4f18e29ccfe5f4c9c06929fe649279dc
rep <reoreoreo922@gmail.com>
2025-10-16 15:38:44 +0900

    20251016v01

---
+++ daisho-day1/script.js
@@ -0,0 +1,9 @@
+const helloBtn=document.getElementByld("helloBtn")
+const message=document.getElementByld("message")
+helloBtn.addEventListener("click",()=>{
+    const hour=new Date().getHours();
+    let greeting="こんにちは！";
+    if(hour<12){
+        greeting=""
+    }
+}
