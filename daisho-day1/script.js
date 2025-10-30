51e5b502aae300015a6ebe466f4ff55cee954463
rep <reoreoreo922@gmail.com>
2025-10-30 13:46:01 +0900

    更新

--- daisho-day1/script.js
+++ daisho-day1/script.js
@@ -4,7 +4,9 @@ helloBtn.addEventListener("click",()=>{
     const hour=new Date().getHours();
     let greeting="こんにちは！";
     if(hour<12){
-        greeting="こんばんわ！";
+        greeting="おはようございます！";
+    }else if(hour>=18){
+        greeting="こんばんは！";
     }
     message.textContent=`${greeting}GitHub Pages の公開に成功しました！`;
 });
